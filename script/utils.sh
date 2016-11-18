97380fa0dbec28a46f936b9433356b0fd1fae093
Jakub Jirutka <jakub@jirutka.cz>
2016-11-18 03:27:15 +0100

    Check minimal required Ruby version in bootstrap

--- script/utils.sh
+++ script/utils.sh
@@ -1,13 +1,21 @@
 # vim: set ts=4:
 
-einfo() {
-	# bold cyan
-	printf '\033[1;36m> %s\033[0m\n' "$@" >&2
-}
-
-ewarn() {
-	# bold yellow
-	printf '\033[1;33m> %s\033[0m\n' "$@" >&2
+# Compares two versions and prints:
+#   -1 if $1 < $2
+#    0 if $1 = $2
+#    1 if $1 > $2
+# $1: left version
+# $2: right version
+compver() {
+	awk -v vera="$1" -v verb="$2" -f - <<-EOF
+		BEGIN {
+			split(vera, a, ".")
+			split(verb, b, ".")
+			for (i = 1; !x && a[i]b[i] != ""; i++)
+				x = (a[i] < b[i]) ? -1 : ((a[i] > b[i]) ? 1 : 0)
+			print x
+		}
+	EOF
 }
 
 die() {
@@ -16,6 +24,16 @@ die() {
 	shift
 	printf '  %s\n' "$@"
 	exit 2
+}
+
+einfo() {
+	# bold cyan
+	printf '\033[1;36m> %s\033[0m\n' "$@" >&2
+}
+
+ewarn() {
+	# bold yellow
+	printf '\033[1;33m> %s\033[0m\n' "$@" >&2
 }
 
 has() {
